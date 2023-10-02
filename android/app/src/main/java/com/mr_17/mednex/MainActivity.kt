516f336744dbf2d038f4f75be8996394bcc160b0
the_mr17 <saptakmanna100@gmail.com>
2023-10-02 10:45:26 +0530

    Adds toolbar

--- android/app/src/main/java/com/mr_17/mednex/MainActivity.kt
+++ android/app/src/main/java/com/mr_17/mednex/MainActivity.kt
@@ -2,12 +2,26 @@ package com.mr_17.mednex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.mr_17.mednex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar.root)
+        supportActionBar?.title = ""
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+    }
+
+    fun setToolbarTitle(title: String) {
+        binding.toolbar.tvTitle.text = title
     }
 }
