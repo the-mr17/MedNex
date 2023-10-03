bc129b74d08f054bfb3d95c84f00c28c86a249fb
Rahul chakraborty <rahulchakraborty337@gmail.com>
2023-10-03 23:19:01 +0530

    dummy commit

--- web/app/api/user/[id]/route.js
+++ web/app/api/user/[id]/route.js
@@ -4,7 +4,7 @@ import { connectToDB } from "@/utils/database";
 export const GET = async (req, { params }) => {
     try {
         await connectToDB();
-        const user = await User.find({ creator: params.id });
+        const user = await User.find({ username: params.id });
         return new Response(JSON.stringify(user), { status: 200 });
     } catch (error) {
         return new Response("Failed to get User", { status: 500 });
