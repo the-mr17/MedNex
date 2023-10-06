c9556965aa76981bb3b917411da0fe991ba8ef47
Rahul chakraborty <rahulchakraborty337@gmail.com>
2023-10-06 17:12:18 +0530

    fix/ GET method for posts fixed

--- web/app/api/posts/route.js
+++ web/app/api/posts/route.js
@@ -5,10 +5,12 @@ export const GET = async (request) => {
     try {
         await connectToDB();
 
-        const posts = await Post.find({}).populate("author");
+        const posts = await Post.find({});
 
         return new Response(JSON.stringify(posts), { status: 200 });
     } catch (error) {
-        return new Response("Failed to fetch all posts", { status: 500 });
+        return new Response("Failed to fetch all posts " + error, {
+            status: 500,
+        });
     }
 };
