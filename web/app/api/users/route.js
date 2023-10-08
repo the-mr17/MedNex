88de0c746ac561f29f6c1fe4d97f31327d5c1a01
Rahul chakraborty <rahulchakraborty337@gmail.com>
2023-10-08 12:40:50 +0530

    better error handling in user upload

--- web/app/api/users/route.js
+++ web/app/api/users/route.js
@@ -16,10 +16,21 @@ export const POST = async (req) => {
         await newUser.save();
         return new Response(JSON.stringify(newUser), { status: 201 });
     } catch (error) {
+        let message = "Failed to create user";
+
+        if (error.code == 11000) {
+            message = Object.keys(error.keyValue)[0] + " already exists!";
+        } else if (error.name == "ValidationError") {
+            for (const field in error.errors) {
+                message += ` ${field}: ${error.errors[field].message}`;
+            }
+        }
         const err = {
-            message: "Failed to create User",
-            error,
+            message: message,
+            errorDetails: error.message,
+            fullError: error,
         };
+        console.log(message);
         return new Response(JSON.stringify(err), { status: 500 });
     }
 };
@@ -30,7 +41,7 @@ export const GET = async (req) => {
         const user = await User.find();
         return new Response(JSON.stringify(user), { status: 200 });
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
         return new Response("Failed to get Users", { status: 500 });
     }
 };
--- web/models/user.js
+++ web/models/user.js
@@ -3,25 +3,25 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
     email: {
         type: String,
-        unique: [true, "Email already exists!"],
-        required: [true, "Email is required!"],
+        unique: [true, "\nEmail already exists!"],
+        required: [true, "\nEmail is required!"],
     },
     fullname: {
         type: String,
-        required: [true, "Full name is required!"],
+        required: [true, "\nFull name is required!"],
     },
     username: {
         type: String,
-        required: [true, "Username is required!"],
+        required: [true, "\nUsername is required!"],
         match: [
             /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-            "Username invalid, it should contain 8-20 alphanumeric characters!",
+            "\nUsername invalid, it should contain 8-20 alphanumeric characters!",
         ],
-        unique: [true, "username already exists!"],
+        unique: [true, "\nusername already exists!"],
     },
     category: {
         type: String,
-        required: [true, "Please select Patient/Doctor"],
+        required: [true, "\nPlease select Patient/Doctor"],
     },
     userimage: {
         type: String,
